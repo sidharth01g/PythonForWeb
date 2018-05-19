89fc41745fc09639a01277f0f9bdf4bcc743d41e
sidharth01g <sidharth01g@gmail.com>
2018-05-19 23:50:27 +0530

    Get posts in the blog

--- blog_app/app.py
+++ blog_app/app.py
@@ -1,6 +1,6 @@
-from blog_app.models.post import BlogPost
 from blog_app.models.blog import Blog
 from blog_app.configurations.blog_config import BlogConfig
+import pprint as pp
 
 
 def main():
@@ -11,14 +11,9 @@ def main():
     blog_config = BlogConfig(uri=uri, db_name=db_name, collection_name=collection_name)
     blog = Blog(blog_config=blog_config, title='Frivolous logs', author='Enigma')
     blog.create_post()
-
-
-    results = [result for result in
-               BlogPost.find_posts(uri=uri, db_name=db_name, collection_name=collection_name, query={})]
-    print('Query results: ', results)
-
-    result = BlogPost.find_post(uri=uri, db_name=db_name, collection_name=collection_name, query={})
-    print('Query result:', result)
+    results = blog.get_posts()
+    print('Current blog posts:')
+    pp.pprint(results)
 
 
 if __name__ == '__main__':
