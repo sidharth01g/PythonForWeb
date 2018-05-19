b8482abb8ad8a59b451b09565d82fff5d2312e80
sidharth01g <sidharth01g@gmail.com>
2018-05-20 01:35:54 +0530

    Verify is user exists in a private method

--- blog_app/models/menu.py
+++ blog_app/models/menu.py
@@ -6,8 +6,18 @@ class Menu(object):
 
     def __init__(self, blog_config: BlogConfig) -> None:
         self.blog_config = blog_config
-        self.author = input('Author: ')
+        self.authorname = input('Author: ')
+
+        if self._has_account():  # Existing author
+            print('Welcome back, {authorname}!'.format(authorname=self.authorname))
+        else:  # New author
+            a = Author(self.authorname)
+            a.post_to_db(uri=self.blog_config.uri, db_name=self.blog_config.db_name,
+                         collection_name=self.blog_config.collection_name_authors)
+            print('Hello, {authorname}, great to see you here!'.format(authorname=self.authorname))
+
+    def _has_account(self):
         results = Author.find_authors(uri=self.blog_config.uri, db_name=self.blog_config.db_name,
                                       collection_name=self.blog_config.collection_name_authors,
-                                      query={"author": self.author})
-        print(results)
+                                      query={"author": self.authorname})
+        return len(results) > 0
