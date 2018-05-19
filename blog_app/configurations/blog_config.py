87f3220e2ee0bcf709b934357efb72597ce85e26
sidharth01g <sidharth01g@gmail.com>
2018-05-20 01:15:38 +0530

    Add authors collection

--- blog_app/configurations/blog_config.py
+++ blog_app/configurations/blog_config.py
@@ -1,6 +1,7 @@
 class BlogConfig(object):
 
-    def __init__(self, uri: str, db_name: str, collection_name: str) -> None:
+    def __init__(self, uri: str, db_name: str, collection_name_posts: str, collection_name_authors: str) -> None:
         self.uri = uri
         self.db_name = db_name
-        self.collection_name_posts = collection_name
+        self.collection_name_posts = collection_name_posts
+        self.collection_name_authors = collection_name_authors
