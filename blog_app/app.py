33a740dd1273bacbe844214135032d83f6a1cebb
sidharth01g <sidharth01g@gmail.com>
2018-05-19 20:35:49 +0530

    uuid assignment for unspecified post ID

--- blog_app/app.py
+++ blog_app/app.py
@@ -1,15 +1,14 @@
 from blog_app.models.post import BlogPost
 
 
+
 def main():
     uri = 'mongodb://127.0.0.1:27017'
     db_name = 'blog_db'
     collection_name = 'blog_posts'
-    post1 = BlogPost(
-        post_id=1, title='Libelous claim', content='Slander is the word', author='Enigma', blog_id=223)
+    post1 = BlogPost(title='Libelous claim', content='Slander is the word', author='Enigma', blog_id=223)
     post1.post_to_db(uri=uri, db_name=db_name, collection_name=collection_name)
-    post2 = BlogPost(
-        post_id=2, title='Majestic maneuver', content='They pulled off a coup', author='Enigma', blog_id=223)
+    post2 = BlogPost(title='Majestic maneuver', content='They pulled off a coup', author='Enigma', blog_id=223)
     post2.post_to_db(uri=uri, db_name=db_name, collection_name=collection_name)
 
     results = [result for result in
