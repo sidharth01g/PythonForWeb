fc91d71b1fd422d2df88aa5c76c3fbe3e3f8df4c
sidharth01g <sidharth01g@gmail.com>
2018-05-19 22:24:11 +0530

    Run using blogging class

--- blog_app/app.py
+++ blog_app/app.py
@@ -1,14 +1,17 @@
 from blog_app.models.post import BlogPost
+from blog_app.models.blog import Blog
+from blog_app.configurations.blog_config import BlogConfig
 
 
 def main():
     uri = 'mongodb://127.0.0.1:27017'
     db_name = 'blog_db'
     collection_name = 'blog_posts'
-    post1 = BlogPost(title='Libelous claim', content='Slander is the word', author='Enigma', blog_id=223)
-    post1.post_to_db(uri=uri, db_name=db_name, collection_name=collection_name)
-    post2 = BlogPost(title='Majestic maneuver', content='They pulled off a coup', author='Enigma', blog_id=223)
-    post2.post_to_db(uri=uri, db_name=db_name, collection_name=collection_name)
+
+    blog_config = BlogConfig(uri=uri, db_name=db_name, collection_name=collection_name)
+    blog = Blog(blog_config=blog_config, title='Frivolous logs', author='Enigma')
+    blog.create_post()
+
 
     results = [result for result in
                BlogPost.find_posts(uri=uri, db_name=db_name, collection_name=collection_name, query={})]
