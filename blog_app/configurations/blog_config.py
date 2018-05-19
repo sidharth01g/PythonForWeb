f43e1fe2d161eb77d2b36c269ac4732b40558b3d
sidharth01g <sidharth01g@gmail.com>
2018-05-20 01:02:13 +0530

    Refactor config variable

--- blog_app/configurations/blog_config.py
+++ blog_app/configurations/blog_config.py
@@ -3,4 +3,4 @@ class BlogConfig(object):
     def __init__(self, uri: str, db_name: str, collection_name: str) -> None:
         self.uri = uri
         self.db_name = db_name
-        self.collection_name = collection_name
+        self.collection_name_posts = collection_name
