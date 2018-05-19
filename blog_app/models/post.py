6ff4d3c9ffb4ce0a9ac2ea2b5cc9b99b6442c45e
sidharth01g <sidharth01g@gmail.com>
2018-05-20 00:19:10 +0530

    Update return type annotation

--- blog_app/models/post.py
+++ blog_app/models/post.py
@@ -25,14 +25,14 @@ class BlogPost(object):
         db.insert(collection_name=collection_name, data=self.get_dict())
 
     @staticmethod
-    def find_posts(uri: str, db_name: str, collection_name: str, query: Dict) -> List[Dict]:
+    def find_posts(uri: str, db_name: str, collection_name: str, query: Dict) -> List['BlogPost']:
         db = Database(uri=uri, db_name=db_name)
         results = db.find(collection_name=collection_name, query=query)
         results = [BlogPost.wrap_result(result) for result in results]
         return results
 
     @staticmethod
-    def find_post(uri: str, db_name: str, collection_name: str, query: Dict) -> Dict:
+    def find_post(uri: str, db_name: str, collection_name: str, query: Dict) -> 'BlogPost':
         db = Database(uri=uri, db_name=db_name)
         result = db.find_one(collection_name=collection_name, query=query)
         result = BlogPost.wrap_result(result)
